a105c056fa14a9d4bf8c9deb93365b73f971b362
Enes Sarı <bilgi@enessari.com>
2022-01-05 17:39:15 +0300

    Home weather

--- Nysa App/Nysa App/Class/WeatherClass.swift
+++ Nysa App/Nysa App/Class/WeatherClass.swift
@@ -10,7 +10,8 @@ import Foundation
 class WeatherClass{
     var weatherAPI = WeatherAPI()
     var weatherArray : [WeatherModel] = []
-    
+    var weatherCity : String = ""
+    var weatherTemp : String = ""
     
     func urlEdit(url : String, lat : Double, long : Double, token : String) -> String{
         return url + "lat=" + String(lat) + "&lon=" + String(long) + "&appid=" + token + "&units=metric"
--- Nysa App/Nysa App/Controller/HomeViewController.swift
+++ Nysa App/Nysa App/Controller/HomeViewController.swift
@@ -8,20 +8,79 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import CoreLocation
 
-class HomeViewController: UIViewController {
+
+class HomeViewController: UIViewController,CLLocationManagerDelegate{
 
     @IBOutlet var homeTableVİew: UITableView!
     var newspaperClass = NewspaperClass()
+    let locationManager = CLLocationManager()
+    var weatherClass = WeatherClass()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.homeTableVİew.delegate = self
         self.homeTableVİew.dataSource = self
-        navigationController?.navigationBar.backgroundColor = UIColor(named: "AccentColor")
+        setupLocationManager()
     }
-   
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            if location.horizontalAccuracy > 0 {
+                locationManager.stopUpdatingLocation()
+                DispatchQueue.main.async {
+                    AF.request(self.weatherClass.urlEdit(url: self.weatherClass.weatherAPI.weatherURL, lat: location.coordinate.latitude, long: location.coordinate.longitude, token: self.weatherClass.weatherAPI.weatherToken)).responseJSON{ response in
+                        switch response.result{
+                        case .success(let value):
+                            let json = JSON(value)
+                            let wData = WeatherModel(name: json["name"].stringValue, weather: [Weather(main: json["weather"][0]["main"].stringValue, description: json["weather"][0]["description"].stringValue, icon: json["weather"][0]["icon"].stringValue)], main: [Main(temp: json["main"]["temp"].stringValue)])
+                            
+                            self.weatherClass.weatherArray.append(wData)
+                            let temp = self.weatherClass.weatherArray[0].main[0].temp
+                            let index = temp.index(temp.startIndex, offsetBy: 2)
+                            self.weatherClass.weatherCity = self.weatherClass.weatherArray[0].name
+                            self.weatherClass.weatherTemp = "\(String(temp.prefix(upTo: index)))°"
+                            self.homeTableVİew.reloadData()
+                        case .failure(let error):
+                            print(error)
+                            let alert = UIAlertController(title: "Error", message: "Weather API Error", preferredStyle: UIAlertController.Style.alert)
+                            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) {_ in
+                                
+                            }
+                            alert.addAction(okAction)
+                           
+                            self.present(alert, animated: true, completion: nil)
+                            
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+    
+    @objc func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location update failed, \(error)")
+        let alert = UIAlertController(title: "Error", message: "Location update failed", preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) {_ in
+            
+            
+        }
+        alert.addAction(okAction)
+       
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 
 
 }
@@ -35,8 +94,8 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
         if indexPath.row == 0{
             
             let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as! WeatherTableViewCell
-            cell.cityLabel.text = "..."
-            cell.temperatureLabel.text = "..."
+            cell.cityLabel.text = self.weatherClass.weatherCity
+            cell.temperatureLabel.text = self.weatherClass.weatherTemp
             return cell
         }
         else if indexPath.row == 1{
